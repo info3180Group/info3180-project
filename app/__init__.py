b8dd4f031e83bba2dbba6f4609a1ea6d18d7ecd3
kkbrown123 <brownkevonteh@gmail.com>
2025-05-04 01:37:50 -0500

    fixing backend

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
-CORS(app, origins=["http://localhost:5173"])
+CORS(app, origins=["https://info3180-project-l40w.onrender.com/"], supports_credentials=True)
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
